a81753576cbe689fd518559611629763d136c542
MatheusMoreiraSantos <matheusmoreiradossantos45@gmail.com>
2022-05-31 11:25:05 -0300

    Alterações 31/05/2022
    Foram adicionados novos métodos e uma validação para trabalhar com uma nova API, a API 30. Essa API e seus respectivos métodos permitem transportar informações de uma activity para outra.

--- new_android_studio/app/src/main/java/matheus/moreira/conceitosintent/MainActivity.java
+++ new_android_studio/app/src/main/java/matheus/moreira/conceitosintent/MainActivity.java
@@ -1,11 +1,17 @@
 package matheus.moreira.conceitosintent;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,41 +21,53 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button           btnPergunta;
-    private EditText         edtPergunta;
-    private TextView         tvExibiResposta;
-    private TextView         tvAResposta;
-    private ImageButton      imbLimparLinhaPergunta;
-    private static final int REQUEST_CODE = 5;
+    private Button                          btnPergunta;
+    private EditText                        edtPergunta;
+    private TextView                        tvExibiResposta;
+    private TextView                        tvAResposta;
+    private ImageButton                     imbLimparLinhaPergunta;
+    private static final int                REQUEST_CODE = 5;
+    private ActivityResultLauncher<Intent>  activityResultLauncher;
+    //<> Indica uma classe genérica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnPergunta                     = findViewById(R.id.btnPergunta);
-        edtPergunta                     = findViewById(R.id.edtPergunta);
-        tvExibiResposta                 = findViewById(R.id.tvExibirResposta);
-        imbLimparLinhaPergunta          = findViewById(R.id.imbLimparLinhaPergunta);
-        tvAResposta                     = findViewById(R.id.tvAResposta);
+        btnPergunta             = findViewById(R.id.btnPergunta);
+        edtPergunta             = findViewById(R.id.edtPergunta);
+        tvExibiResposta         = findViewById(R.id.tvExibirResposta);
+        imbLimparLinhaPergunta  = findViewById(R.id.imbLimparLinhaPergunta);
+        tvAResposta             = findViewById(R.id.tvAResposta);
+
 
         Bundle extras = getIntent().getExtras();
         String pergunta = "";
-        if(extras != null) {
+        if (extras != null) {
             pergunta = extras.getString("Pergunta");
             tvExibiResposta.setText(pergunta);
         }
-        
+
         btnPergunta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!edtPergunta.getText().toString().isEmpty()){
-                    Intent intent = new Intent(MainActivity.this, RespostaActivity.class);
+                if (!edtPergunta.getText().toString().isEmpty()) {
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                        Intent intent = new Intent(MainActivity.this, RespostaActivity.class);
 
-                    String myString = edtPergunta.getText().toString();
-                    intent.putExtra("Pergunta", myString);
+                        String myString = edtPergunta.getText().toString();
+                        intent.putExtra("Pergunta", myString);
 
-                    startActivityForResult(intent, REQUEST_CODE);
+                        if (!tvExibiResposta.getText().toString().isEmpty()) {
+                            String myResposta = tvExibiResposta.getText().toString();
+                            intent.putExtra("Resposta", myResposta);
+                        }
+
+                        startActivityForResult(intent, REQUEST_CODE);
+                    } else {
+                        openActivityForResult();
+                    }
 
                 } else {
                     Toast.makeText(MainActivity.this, "Por favor insira uma pergunta", Toast.LENGTH_SHORT).show();
@@ -57,13 +75,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        imbLimparLinhaPergunta.setOnClickListener(new View.OnClickListener() {
+        imbLimparLinhaPergunta.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onClick(View view) {
+            public void onClick (View view){
                 edtPergunta.setText("");
-                tvExibiResposta.setText("");
+                tvExibiResposta.setText(""); }
+        });
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    tvExibiResposta.setText(data.getExtras().toString());
+                }
             }
         });
+
     }
 
     @Override
@@ -87,8 +116,22 @@ public class MainActivity extends AppCompatActivity {
                     edtPergunta.setText(perguntaRetornadaDaActivityResposta);
                 }
             }
-
         }
+
+
+
+
+
+    }
+
+    public void openActivityForResult(){
+        //Instanciação dinâmica de objeto;
+        //Desta forma, o objeto é considerado uma variável local;
+        Intent intent = new Intent(MainActivity.this, RespostaActivity.class);
+
+        intent.putExtra("Pergunta", edtPergunta.getText().toString());
+
+        activityResultLauncher.launch(intent);
 
     }
 
@@ -103,16 +146,26 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
