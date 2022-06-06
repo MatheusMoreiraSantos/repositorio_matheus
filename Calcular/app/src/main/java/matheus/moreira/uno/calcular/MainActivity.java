c1da5bf316a5b122fd8352781aaa73c0e15edf76
MatheusMoreiraSantos <matheusmoreiradossantos45@gmail.com>
2022-06-06 11:25:10 -0300

    Alterações Calcular 06/06/2022
    Demos segmento a implementação da lista do Spinner, codificando sues respectivos métodos. Foram feitos ajustes no layout, incluindo img = .

--- Calcular/app/src/main/java/matheus/moreira/uno/calcular/MainActivity.java
+++ Calcular/app/src/main/java/matheus/moreira/uno/calcular/MainActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String DIVISAO         = "Divisão";
+    private static final String MULTIPLICACAO   = "Multiplição";
+    private static final String SOMA            = "Soma";
+    private static final String SuBTRACAO       = "Subtração";
     private EditText edtOperando, edtOperando2;
     private TextView tvOpcao, tvOperacao, tvResultado;
-    private View spiOpcoes;
-    private ImageView imgOperacao, imageView2;
-
+    private Spinner spiOpcoes;
+    private ImageView imgOperacao, imgOperacao2;
+    private Button btnCalcular;
 
 
 
@@ -22,14 +32,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtOperando     = findViewById(R.id.edtOperando1);
-        edtOperando2    = findViewById(R.id.edtOperando2);
-        tvOpcao         = findViewById(R.id.tvOpcao);
-        tvOperacao      = findViewById(R.id.tvOperacao);
-        tvResultado     = findViewById(R.id.tvResultado);
-        spiOpcoes       = findViewById(R.id.spiOpcoes);
-        imgOperacao     = findViewById(R.id.imgOperacao);
-        imageView2      = findViewById(R.id.imageView2);
+        edtOperando         = findViewById(R.id.edtOperando1);
+        edtOperando2        = findViewById(R.id.edtOperando2);
+        tvOpcao             = findViewById(R.id.tvOpcao);
+        tvOperacao          = findViewById(R.id.tvOperacao);
+        tvResultado         = findViewById(R.id.tvResultado);
+        spiOpcoes           = findViewById(R.id.spiOpcoes);
+        imgOperacao         = findViewById(R.id.imgOperacao);
+        imgOperacao2        = findViewById(R.id.imgOperacao2);
+        btnCalcular         = findViewById(R.id.btnCalcular);
+
+        //Cria/instancia os elementos contidos no strings.xml; Lista de operações matemáticas criada;
+        ArrayAdapter<String> adapterOpcoesMatematicas = new ArrayAdapter<String>(this
+                , android.R.layout.simple_spinner_item
+                , getResources().getStringArray(R.array.operacoes_matematica));
+        adapterOpcoesMatematicas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spiOpcoes.setAdapter(adapterOpcoesMatematicas);
+        spiOpcoes.setOnItemSelectedListener(this);
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Pega a opção selecionada no SPINNER
+                String opcaoSelecionada = spiOpcoes.getSelectedItem().toString();
+
+                if(opcaoSelecionada == DIVISAO){
+
+                }
+
+
+            }
+        });
 
 
 
@@ -37,6 +72,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+
+
+    //Adapter tem a ver com o adapter criado anteriormente e a View com o Spinner e seu objeto;
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        
+
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
