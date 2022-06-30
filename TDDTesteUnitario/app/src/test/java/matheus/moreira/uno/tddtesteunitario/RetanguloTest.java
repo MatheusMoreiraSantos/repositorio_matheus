17825593757dcdbad88870ce773defbe95c00c73
MatheusMoreiraSantos <matheusmoreiradossantos45@gmail.com>
2022-06-30 11:26:36 -0300

    Alterações 30/06/2022
    Codificação de alguns testes

--- TDDTesteUnitario/app/src/test/java/matheus/moreira/uno/tddtesteunitario/RetanguloTest.java
+++ TDDTesteUnitario/app/src/test/java/matheus/moreira/uno/tddtesteunitario/RetanguloTest.java
@@ -4,11 +4,32 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import matheus.moreira.uno.tddtesteunitario.forma.Retangulo;
+
 public class RetanguloTest {
+    private static final int POSICAO_ZERO = 0;
+    private static final int POSICAO_UM = 1;
 
 
     @Test
-    public void area(){
+    public void deveriaCalcularAreaDoRetangulo(){
+
+        Retangulo r = new Retangulo();
+
+        r.setMedidas(POSICAO_ZERO, POSICAO_UM, 3, 2);
+
+
+        double area01 = 6;
+        double area02 = 750;
+        double area03 = 36;
+
+
+        double area = medida1 * medida2;
+        
+
+
+        assertEquals();
+
 
     }
 
