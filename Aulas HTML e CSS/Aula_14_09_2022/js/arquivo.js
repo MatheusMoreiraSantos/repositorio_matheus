853787eae43db9dc68ff1220dba185baab93e22d
MatheusMoreiraSantos <matheusmoreiradossantos45@gmail.com>
2022-09-14 11:19:03 -0300

    Aula 14.09.2022
    Introdução ao Javascript

---
+++ Aulas HTML e CSS/Aula_14_09_2022/js/arquivo.js
@@ -0,0 +1,22 @@
+//alert("Olá mundo!");
+
+/*{ 
+document.write("<h1>Este é um cabeçalho</h1>");
+document.write("<p>Isto é um parágrafo</p>");
+document.write("<p>Este é outro parágrafo</p>");
+}  
+
+//window.prompt();
+nome=prompt("Qual seu nome?"); 
+
+
+
+document.write("Alo mundo em JavaScript");
+
+
+document.write("<h1>Minha Página</h1>");
+document.write("<h3>"+nome+", seja bem-vindo a minha página</h3>");*/
+
+var a= 25 + 78;
+
+alert(""+a+"");
